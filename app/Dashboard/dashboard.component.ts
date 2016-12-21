ea97068b8b0b48f05debaadac2957345411c04cd
Andy Hoch <andy.hoch@parivedasolutions.com>
2016-12-21 15:50:07 -0600

    Add Dashboard Component Stub and introduce Routing Module

---
+++ app/Dashboard/dashboard.component.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by andyhoch on 12/21/16.
+ */
+import { Component, OnInit } from '@angular/core';
+import { SportsDataService } from '../Services/sports-data.service';
+
+@Component({
+    moduleId: module.id,
+    selector: 'fantasy-sports-dashboard',
+    styleUrls: ['dashboard.component.css'],
+    templateUrl: 'dashboard.component.html'
+})
+export class DashboardComponent{
+
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,21 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
 
 import { AppComponent }  from './app.component';
+import { DashboardComponent } from './Dashboard/dashboard.component';
+import { AppRoutingModule } from './app-routing.module';
+import { SportsDataService } from './Services/sports-data.service';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule,
+                  HttpModule,
+                  FormsModule,
+                  AppRoutingModule],
+  declarations: [ AppComponent,
+                  DashboardComponent],
+  providers: [SportsDataService],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
